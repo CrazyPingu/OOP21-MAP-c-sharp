c349d2feabe70a4b82b14d2264668af7b9fd9e0e
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-06-27 23:25:34 +0200

    Add Room and RoomImpl

---
+++ Samuele De Tuglie/Room/Room.cs
@@ -0,0 +1,31 @@
+﻿using Utilities;
+
+namespace Room
+{
+    /// <summary>
+    /// Interface for the creation of a room
+    /// </summary>
+    public interface Room
+    {
+        /// <summary>
+        /// Method to control if the player is on the door
+        /// </summary>
+        /// <returns>True if the player is on the door otherwise false</returns>
+        bool PlayerOnDoor();
+
+        /// <summary>
+        /// Method to remove an object from the game
+        /// </summary>
+        /// <param name="pos">The position of the object to be removed</param>
+        void UpdatePosition(Pair<int, int> oldPos, Pair<int, int> newPos);
+
+        /// <summary>
+        /// Method to move an object in the game
+        /// </summary>
+        /// <param name="oldPos">The current position of the object</param>
+        /// <param name="newPos">The position that the object will go on</param>
+        void RemoveObject(Pair<int, int> pos);
+
+
+    }
+}
---
+++ Samuele De Tuglie/RoomConstant.cs
@@ -0,0 +1,45 @@
+﻿namespace Utilities
+{
+    public class RoomConstant
+    {
+        public static Color ATTACK_HIGHLIGHT = Color.Red;
+        public static Color MOVE_HIGHLIGHT = Color.Blue;
+        public static Color ENEMY_RANGE = Color.FromArgb(153, 50, 204);
+
+        public static Color BASIC_CELL_COLOR = Color.Gray;
+        public static Color DOOR_CELL_COLOR = Color.FromArgb(110, 38, 14);
+
+        public static int FORBIDDEN_ZOMBIE_SPAWN = 3;
+
+        
+        public static SimpleEnemy SearchEnemy(Pair<int, int> pos, List<SimpleEnemy> enemyList)
+        {
+            if (enemyList != null && enemyList.Any())
+            {
+                foreach (SimpleEnemy x in enemyList)
+                {
+                    if (x.Pos.Equals(pos))
+                    {            
+                        return x;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static Artefact SearchArtefact(Pair<int, int> pos, List<Artefact> artefactList)
+        {
+            if (artefactList != null && artefactList.Any())
+            {
+                foreach (Artefact x in artefactList)
+                {
+                    if (x.Pos.Equals(pos))
+                    {
+                        return x;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
