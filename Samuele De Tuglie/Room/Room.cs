e8596b46bd515c03cf657881b7647f2bdd1683bd
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-07-01 21:06:01 +0200

    Fix room name class

--- Samuele De Tuglie/Room/Room.cs
+++ Samuele De Tuglie/Room/Room.cs
@@ -4,36 +4,85 @@ using Button;
 namespace Room
 {
     /// <summary>
-    /// Interface for the creation of a room
+    /// Class that rappresent a Room
     /// </summary>
-    public interface Room
+    public class Room : IRoom
     {
-        /// <summary>
-        /// Method to control if the player is on the door
-        /// </summary>
-        /// <returns>True if the player is on the door otherwise false</returns>
-        bool PlayerOnDoor();
+        public Pair<int, int> Size { get; }
+        public Dictionary<Pair<int, int>, GameButton> Cells { get; }
+        public Player Player { get; }
+        public List<Pair<int, int>> Door { get; }
+        public List<SimpleEnemy> EnemyList { get; set; }
+        public List<Artefact> ArtefactList { get; set; }
+        public List<Obstacle> ObstacleList { get; set; }
 
         /// <summary>
-        /// Method to add a button to the grid
+        /// Constructor of the Room
         /// </summary>
-        /// <param name="pos">The position of the button</param>
-        /// <param name="button">The button to be added</param>
-        void AddButtonToCells(Pair<int, int> pos, GameButton button);
+        /// <param name="size">The size of the room</param>
+        /// <param name="Player">The player of the game</param>
+        /// <param name="newPosPlayer">The new position of the player</param>
+        public RoomImpl(Pair<int, int> size, Player Player, Pair<int, int> newPosPlayer)
+        {
+            Size = size;
+            Player = Player;
+            Door = GenerateDoor();
+        }
 
         /// <summary>
-        /// Method to remove an object from the game
+        /// Method to generate the door
         /// </summary>
-        /// <param name="pos">The position of the object to be removed</param>
-        void UpdatePosition(Pair<int, int> oldPos, Pair<int, int> newPos);
+        /// <returns>A list of pair that rappresent the door</returns>
+        private List<Pair<int, int>> GenerateDoor()
+        {
+            List<Pair<int, int>> tmp = new List<Pair<int, int>>();
+            tmp.Add(new Pair<int, int>(Size.X - 1, Size.Y / 2));
+            if (Size.Y % 2 == 0)
+            {
+                tmp.Add(new Pair<int, int>(Size.X - 1, Size.Y / 2 - 1));
+            }
+            return tmp;
+        }
 
-        /// <summary>
-        /// Method to move an object in the game
-        /// </summary>
-        /// <param name="oldPos">The current position of the object</param>
-        /// <param name="newPos">The position that the object will go on</param>
-        void RemoveObject(Pair<int, int> pos);
+        /// <inheritdoc />
+        public bool PlayerOnDoor()
+        {
+            return Door.Contains(Player.Pos);
+        }
 
+        /// <inheritdoc />
+        public void RemoveObject(Pair<int, int> pos)
+        {
+            if (RoomConstant.SearchEnemy(pos, EnemyList) != null)
+            {
+                EnemyList.Remove(RoomConstant.SearchEnemy(pos, EnemyList));
+            }
+            else if (RoomConstant.SearchArtefact(pos, ArtefactList) != null)
+            {
+                ArtefactList.Remove(RoomConstant.SearchArtefact(pos, ArtefactList));
+            }
+            else if (Player.Pos.Equals(pos))
+            {
+                Player = null;
+            }
+        }
 
+        /// <inheritdoc />
+        public void UpdatePosition(Pair<int, int> oldPos, Pair<int, int> newPos)
+        {
+            if (RoomConstant.SearchEnemy(oldPos, EnemyList) != null)
+            {
+                RoomConstant.SearchEnemy(oldPos, EnemyList).Pos = newPos;
+            }
+            else if (Player.Pos.Equals(oldPos))
+            {
+                this.Player.Pos = newPos;
+            }
+        }
+
+        public void AddButtonToCells(Pair<int, int> pos, GameButton button)
+        {
+            Cells.Add(pos, button);
+        }
     }
 }
