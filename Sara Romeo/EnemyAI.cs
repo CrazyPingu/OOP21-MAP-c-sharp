fa16bed54b0fde5c0b4cdca938820890d96972a8
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-07-02 15:49:08 +0200

    Add methods and doc to interface

--- Sara Romeo/EnemyAI.cs
+++ Sara Romeo/EnemyAI.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOP21_MAP_c_sharp.OOP21_MAP_c_sharp.Sara_Romeo
+﻿namespace EnemyAI
 {
-    internal class Class1
+    public class EnemyAi : IEnemyAI
     {
     }
 }
--- Sara Romeo/IEnemyAI.cs
+++ Sara Romeo/IEnemyAI.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOP21_MAP_c_sharp.OOP21_MAP_c_sharp.Sara_Romeo
+﻿namespace EnemyAI
 {
-    internal interface Interface1
+    /// <summary>
+    /// Methods to define enemies behaviors during their turn.
+    /// </summary>
+    internal interface IEnemyAI
     {
+
+        /// <summary>
+        /// Enemy automated movement.
+        /// </summary>
+        /// <param name="enemy">enemy of which is needed to change position</param>
+        /// <returns> the new calculated position for the enemy</returns>
+        Tuple<int, int> move(SimpleEnemy enemy);
+
+        /// <summary>
+        /// Checks if player is reachable by the enemy attack.
+        /// </summary>
+        /// <param name="enemy">entity of which is needed to know the attackable area</param>
+        /// <param name="player">entity to be found whether or not is in the attackable area</param>
+        /// <param name="roomSize">size of the current room</param>
+        /// <returns> true if the player passed is in the attack area of the enemy passed</returns>
+        bool isPlayerInAttackArea(SimpleEnemy enemy, Player player, Tuple<int, int> roomSize);
+
     }
 }
