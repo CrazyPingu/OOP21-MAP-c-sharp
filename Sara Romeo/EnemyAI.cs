c01dbb3f92bd3ea995f696f46e202602c8633678
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-07-02 16:11:38 +0200

    Create constructor and methods

--- Sara Romeo/EnemyAI.cs
+++ Sara Romeo/EnemyAI.cs
@@ -1,6 +1,49 @@
-﻿namespace EnemyAI
+﻿
+/// <summary>
+/// Implementation of enemies behaviors.
+/// </summary>
+namespace EnemyAI
 {
-    public class EnemyAi : IEnemyAI
+    public class EnemyAI : IEnemyAI
     {
+
+        private Room.Room _room;
+
+        public EnemyAI(Room.Room room) => _room = room;
+
+        /// <summary>
+        /// Updates the room in which the enemy AI is placed.
+        /// </summary>
+        /// <param name="room">current room</param>
+        public void updateAIRoom(readonly Room.Room room)
+        {
+            _room = room;
+        }
+
+
+
+
+        /// <inheritdoc />
+        public Tuple<int, int> move(SimpleEnemy enemy)
+        {
+            throw new NotImplementedException();
+        }
+        /// <inheritdoc />
+        public bool isPlayerInAttackArea(SimpleEnemy enemy, Player player, Tuple<int, int> roomSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cell">from which is calculated the distance</param>
+        /// <param name="playerPos">player's current position</param>
+        /// <returns>the distance between player's current position and given cell</returns>
+        private int calculateDistanceFromPlayer(final Pair<Integer, Integer> cell, final Pair<Integer, Integer> playerPos)
+        {
+            
+        }
+
     }
 }
