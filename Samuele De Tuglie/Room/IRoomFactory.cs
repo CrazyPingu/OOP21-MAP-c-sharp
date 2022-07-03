b865d96ae9ed55479c34c61cc4dea588e115232e
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-07-03 18:19:33 +0200

    Add room factory

--- Samuele De Tuglie/Room/IRoomFactory.cs
+++ Samuele De Tuglie/Room/IRoomFactory.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOP21_MAP_c_sharp.OOP21_MAP_c_sharp.Samuele_De_Tuglie.Room
+﻿namespace RoomArea
 {
-    internal class IRoomFactory
+    /// <summary>
+    /// Interface for the creation of a room factory
+    /// </summary>
+    public interface IRoomFactory
     {
+        /// <summary>
+        /// Method to create a small room
+        /// </summary>
+        /// <returns>A small room</returns>
+        Room CreateSmallRoom();
+
+        /// <summary>
+        /// Method to create a big room
+        /// </summary>
+        /// <returns>A big room</returns>
+        Room CreateBigRoom();
+
+        /// <summary>
+        /// Method to create a medium room
+        /// </summary>
+        /// <returns>A medium room</returns>
+        Room CreateMediumRoom();
     }
 }
