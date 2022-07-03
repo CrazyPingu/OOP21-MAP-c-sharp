82aaea02c38e194d61111115d7a21efed63c23ae
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-07-03 16:50:43 +0200

    created movement interface and movement placeholder class

---
+++ Luca Babboni/Movement/IMovement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovementStrategy
+{
+
+
+    /// <summary>
+    /// Interface that declare methods for the movement sistem. Represent the
+    /// "Context" of the Strategy interface.
+    /// </summary>
+    public interface IMovement
+    {
+        /// <summary>
+        /// Given the a position that represent the current utilizer position return a list 
+        /// that rapresent the possible position it can reach. 
+        /// </summary>
+        /// <param name="pos"> The current position in the map grid</param>
+        /// <param name="size">The size of the map grid</param>
+        /// <returns></returns>
+        List<Tuple<int, int>> reachableCells(Tuple<int, int> pos, Tuple<int, int> size);
+    }
+
+}
+
---
+++ Luca Babboni/Movement/Movement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop21_MAP_Csharp.OOP21_MAP_c_sharp.Luca_Babboni.Movement
+{
+    internal class Movement
+    {
+    }
+}
