9a45fb5cb6240d783cc270b7353bdf6d038504e9
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-07-03 14:30:16 +0200

    Implement factory impl

--- Sara Romeo/EnemyFactory.cs
+++ Sara Romeo/EnemyFactory.cs
@@ -2,5 +2,39 @@
 {
     internal class EnemyFactory : IEnemyFactory
     {
+        /// <inheritdoc />
+        public SimpleEnemy CreateZombieStick(Tuple<int, int> pos)
+        {
+            return new SimpleEnemy(new SimpleLifeSystem(Health.LOW_HEALTH.getHealth()), pos, new WeaponFactoryImpl().CreateStick(),
+                new MovementFactoryImpl().CreateStepMovement(), "zombie stick", EntityTexture.ZOMBIE_STICK);
+        }
+
+        /// <inheritdoc />
+        public SimpleEnemy CreateZombieAxe(Tuple<int, int> pos)
+        {
+            return new SimpleEnemy(new SimpleLifeSystem(Health.LOW_HEALTH.getHealth()), pos, new WeaponFactoryImpl().CreateAxe(),
+                new MovementFactoryImpl().CreateStepMovement(), "zombie axe", EntityTexture.ZOMBIE_AXE);
+        }
+
+        /// <inheritdoc />
+        public SimpleEnemy CreateZombieDagger(Tuple<int, int> pos)
+        {
+            return new SimpleEnemy(new SimpleLifeSystem(Health.MID_HEALTH.getHealth()), pos, new WeaponFactoryImpl().CreateDagger(),
+                new MovementFactoryImpl().CreateRunMovement(), "zombie dagger", EntityTexture.ZOMBIE_DAGGER);
+        }
+
+        /// <inheritdoc />
+        public SimpleEnemy CreateZombieTube(Tuple<int, int> pos)
+        {
+            return new SimpleEnemy(new SimpleLifeSystem(Health.HIGH_HEALTH.getHealth()), pos, new WeaponFactoryImpl().CreateTube(),
+                new MovementFactoryImpl().CreateStepMovement(), "zombie tube", EntityTexture.ZOMBIE_TUBE);
+        }
+
+        /// <inheritdoc />
+        public SimpleEnemy CreateZombieGun(Tuple<int, int> pos)
+        {
+            return new SimpleEnemy(new SimpleLifeSystem(Health.HIGH_HEALTH.getHealth()), pos, new WeaponFactoryImpl().CreateGun(),
+                new MovementFactoryImpl().CreateRunMovement(), "zombie gun", EntityTexture.ZOMBIE_GUN);
+        }
     }
 }
