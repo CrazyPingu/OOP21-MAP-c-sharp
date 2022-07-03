2afafae516e7334c6dc4dc4cc7eb7330386cf6e6
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-07-03 18:19:00 +0200

    Add type of room

---
+++ Samuele De Tuglie/Room/RoomType/BigRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RoomArea;
+
+namespace RoomType
+{
+    /// <summary>
+    /// Biggest room possible
+    /// </summary>
+    internal class BigRoom : Room
+    {
+        /// <summary>
+        /// Constructor of the big room
+        /// </summary>
+        /// <param name="size">The size of the room</param>
+        /// <param name="player">The player of the game</param>
+        public BigRoom(Tuple<int, int> size, Player player) : base(size, player, new Tuple<int, int>(2, size.Item2 / 2)) { }
+    }
+}
---
+++ Samuele De Tuglie/Room/RoomType/SmallRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RoomArea;
+
+namespace RoomType
+{
+    /// <summary>
+    /// The smaller room
+    /// </summary>
+    public class SmallRoom : Room
+    {
+        /// <summary>
+        /// The constructor of the small room
+        /// </summary>
+        /// <param name="size">The size of the room</param>
+        /// <param name="player">The player of the game</param>
+        public SmallRoom(Tuple<int, int> size, Player player) : base(size, player, new Tuple<int, int>(1, size.Item2 / 2)) { }
+    }
+}
