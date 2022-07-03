8de9356b3b18e05d1133dc8bf64f71271a366bb8
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2022-07-03 17:14:38 +0200

    Create interface for the room factory

---
+++ Samuele De Tuglie/Room/IRoomFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP21_MAP_c_sharp.OOP21_MAP_c_sharp.Samuele_De_Tuglie.Room
+{
+    internal class IRoomFactory
+    {
+    }
+}
