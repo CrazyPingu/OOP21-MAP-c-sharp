95a0d8984204180c9599937ba34cee43e53d37d1
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-07-03 14:50:24 +0200

    Create interface methods

--- Sara Romeo/Weapon/IWeapon.cs
+++ Sara Romeo/Weapon/IWeapon.cs
@@ -1,6 +1,16 @@
 ﻿namespace Weapon
 {
-    internal interface IWeapon
+    /// <summary>
+    /// Interface that declares methods for the weapon. Represent the "Context" of Factory interface.
+    /// </summary>
+    public interface IWeapon
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pos">current pos</param>
+        /// <param name="size"></param>
+        /// <returns>list of available cells to attack based on shooting strategy applied.</returns>
+        List<Tuple<int, int>> getAttackArea(Tuple<int, int> pos, Tuple<int, int> size);
     }
 }
