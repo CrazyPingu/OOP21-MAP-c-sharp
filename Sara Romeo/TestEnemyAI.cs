59cd7bd9870b7b2e20a0a238df83e565c20791e8
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-07-03 11:19:55 +0200

    Create test class

--- Sara Romeo/TestEnemyAI.cs
+++ Sara Romeo/TestEnemyAI.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
-namespace OOP21_MAP_c_sharp.OOP21_MAP_c_sharp.Sara_Romeo
+namespace Test
 {
-    internal class Class1
+    [TestFixture]
+    public class TestEnemyAI
     {
+        
     }
 }
