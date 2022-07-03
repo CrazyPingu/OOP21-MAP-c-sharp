fe49dee2b19619b665bbf1e6227ff63cbf63cbcf
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-07-03 14:50:54 +0200

    Implement methods

--- Sara Romeo/Weapon/Weapon.cs
+++ Sara Romeo/Weapon/Weapon.cs
@@ -1,6 +1,29 @@
 ﻿namespace Weapon
 {
-    internal class Weapon
+    /// <summary>
+    /// Create the weapon designed by the passed value implementing the weapon interface.
+    /// </summary>
+    public class Weapon
     {
+        public int Damage { get; }
+        public String Name { get; }
+        private readonly Strategy _shootingStrategy;
+
+        /**
+         * @param damage           is the damage that the weapon can inflict
+         * @param shootingStrategy is the area within the weapon can reach
+         */
+        public Weapon(int damage, String name, Strategy shootingStrategy)
+        {
+            Damage = damage;
+            Name = name;
+            _shootingStrategy = shootingStrategy;
+        }
+
+        /// <inheritdoc />
+        public List<Tuple<int, int>> getAttackArea(Tuple<int, int> pos, Tuple<int, int> size)
+        {
+            return _shootingStrategy.execute(pos, size);
+        }
     }
 }
