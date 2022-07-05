7cb55b3770d533ea9fb36a5bcbc8517a051942cf
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-07-05 10:51:49 +0200

    created class Entity

---
+++ Luca Babboni/GameObject/Entity/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop21_MAP_Csharp.OOP21_MAP_c_sharp.Luca_Babboni.GameObject.Entity
+{
+    /// <summary>
+    ///Class to incapsulate an entity behaviur. This class will be extends to create
+    /// more complex Entity.
+    /// Implements all the common method for a simple game object able to hold a
+    /// weapon, move and take damage. This class is used for the enemy
+    /// implementation.
+    /// 
+    /// This class is only partially traslated from jata to c#. It will implement only the movement 
+    /// feature
+    /// </summary>
+    internal class Entity
+    {
+
+    }
+}
