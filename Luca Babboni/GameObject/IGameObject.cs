52a2ae3f18a18c9c913e2e0fa108c1f130ae989e
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-07-05 10:31:25 +0200

    created interface IGameObject

---
+++ Luca Babboni/GameObject/IGameObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop21_MAP_Csharp.OOP21_MAP_c_sharp.Luca_Babboni.GameObject
+{
+    /// <summary>
+    /// This class declare the common get for every object in game that can be
+    /// displayed in the game grid.
+    /// </summary>
+    public interface IGameObject
+    {
+        Tuple<int, int> Pos { get; }
+
+        String name { get; } 
+
+    }
+}
