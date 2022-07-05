cb45fd523ada65002452abadabf16d06864ea07f
Luca Babboni <93348024+ElektroDuck@users.noreply.github.com>
2022-07-05 10:01:56 +0200

    completed the Movement system classes

--- Luca Babboni/Movement/IMovement.cs
+++ Luca Babboni/Movement/IMovement.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MovementStrategy
 {
-
-
     /// <summary>
     /// Interface that declare methods for the movement sistem. Represent the
     /// "Context" of the Strategy interface.
@@ -21,8 +16,7 @@ namespace MovementStrategy
         /// <param name="pos"> The current position in the map grid</param>
         /// <param name="size">The size of the map grid</param>
         /// <returns></returns>
-        List<Tuple<int, int>> reachableCells(Tuple<int, int> pos, Tuple<int, int> size);
+        List<Tuple<int, int>> ReachableCells(Tuple<int, int> pos, Tuple<int, int> size);
     }
-
 }
 
--- Luca Babboni/Movement/Movement.cs
+++ Luca Babboni/Movement/Movement.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Strategy;
 
-namespace oop21_MAP_Csharp.OOP21_MAP_c_sharp.Luca_Babboni.Movement
+namespace MovementStrategy
 {
-    internal class Movement
+    /// <summary>
+    /// This class define the method for the movement implementation. This class is a
+    /// part of the stratey pattern, represent the "Context" of the Strategy interface.
+    /// </summary>
+    public class Movement : IMovement
     {
+        private readonly IStrategy _movingStrategy;
+
+        /// <summary>
+        /// set the movingStrategy of the movement system to the desired one passed in input as an argument.
+        /// </summary>
+        /// <param name="movingStrategy">the strategy of the movement system</param>
+        public Movement(IStrategy movingStrategy) 
+        {
+            _movingStrategy = movingStrategy;
+        }
+
+        public List<Tuple<int, int>> ReachableCells(Tuple<int, int> pos, Tuple<int, int> size)
+        {
+            return _movingStrategy.Execute(pos, size);
+        }
     }
 }
