a6231b183eab40f044345f675a2110b3fa794f0f
Federico Bartolini <federico.bartolini6@studio.unibo.it>
2022-07-06 14:08:18 +0200

    Fix import Libraries

--- Federico Bartolini/Obstacle/Obstacle.cs
+++ Federico Bartolini/Obstacle/Obstacle.cs
@@ -1,10 +1,6 @@
 ﻿using GameObject;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace ObstacleObject
