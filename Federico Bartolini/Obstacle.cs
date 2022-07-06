b91fa615444fa3c57dfc8bf80d379b51af96ba21
Federico Bartolini <federico.bartolini6@studio.unibo.it>
2022-07-06 13:50:28 +0200

    Create Obstacle class

---
+++ Federico Bartolini/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using GameObject;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ObstacleObject
+{
+    /// <summary>
+    /// The class of the obstacle created in Game
+    /// </summary>
+    public class Obstacle : IGameObject
+    { 
+
+        /// <summary>
+        ///  The position of the obstacle
+        /// </summary>
+        Tuple<int, int> IGameObject.Pos { get; }
+
+        /// <summary>
+        /// The name of the obstacle
+        /// </summary>
+        string IGameObject.Name { get; }
+    }
+}
