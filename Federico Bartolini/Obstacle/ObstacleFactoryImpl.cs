a90594d6a6d58608909c9db5cf40e59ccea142e6
Federico Bartolini <federico.bartolini6@studio.unibo.it>
2022-07-06 14:37:56 +0200

    Update Documentation

--- Federico Bartolini/Obstacle/ObstacleFactoryImpl.cs
+++ Federico Bartolini/Obstacle/ObstacleFactoryImpl.cs
@@ -3,13 +3,18 @@
 namespace ObstacleObject
 
 {
+    /// <summary>
+    /// The class that implements the interface ObstacleFactory
+    /// </summary>
     public class ObstacleFactoryImpl : IObstacleFactory
     {
+        /// <inheritdoc/>
         public Obstacle CreatePebble(Tuple<int, int> pos)
         {
             return new Obstacle(pos, "rock", null);
         }
 
+        /// <inheritdoc/>
         public Obstacle CreateRock(Tuple<int, int> pos)
         {
             return new Obstacle(pos, "pebble", null);
