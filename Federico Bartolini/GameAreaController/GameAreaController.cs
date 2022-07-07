0d42ab2864f7aa224925d1597b5bc4081a698d1c
Federico Bartolini <federico.bartolini6@studio.unibo.it>
2022-07-07 16:03:46 +0200

    Create GameAreaController

---
+++ Federico Bartolini/GameAreaController/GameAreaController.cs
@@ -0,0 +1,51 @@
+﻿using EntityObject;
+using ObstacleObject;
+using RandomGenerator;
+using RoomArea;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace GameAreaController
+{
+    public class GameAreaController
+    {
+
+        private Player _player { get; }
+        private List<SimpleEnemy> EnemyList { get; }
+        private Room room;
+        private List<Obstacle> ObstacleList;
+
+        public GameAreaController(Player player)
+        {
+            _player = player;
+            room = new RoomFactory(_player).CreateMediumRoom();
+            ObstacleList = room.ObstacleList;
+            EnemyList = room.EnemyList;
+
+        }
+
+
+
+
+
+
+
+
+        public void RemoveGameObject(Tuple<int, int> pos)
+        {
+            if (pos != null && RoomConstant.CellsOccupated(ObstacleList, _player, EnemyList, pos))
+            {
+                if (RoomConstant.SearchEnemy(pos, EnemyList) != null)
+                {
+                    EnemyList.Remove(RoomConstant.SearchEnemy(pos, EnemyList));
+                }
+            }
+        }
+
+
+    }
+}
